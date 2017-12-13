e3bb07e06d92f3abf7512ba1da710add8151b792
Kayla Webb <kaylarwebb@gmail.com>
2017-12-13 17:01:31 -0500

    Clean up

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -27,7 +27,7 @@ export default class  Gallery extends React.Component {
 
     toggle() {
         this.setState({
-            showGallery: true
+            showGallery: true 
         })
     }
     
